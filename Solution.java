877f0a3fe272f791b9928a115d8296c78da4abb6
An4or <anchor_gomel@tut.by>
2017-06-26 23:52:15 +0300

    Initial commit

---
+++ Solution.java
@@ -0,0 +1,34 @@
+
+
+import java.util.*;
+
+/* 
+Машину на СТО не повезем!
+*/
+public class Solution {
+    public static enum Wheel {
+        FRONT_LEFT,
+        FRONT_RIGHT,
+        BACK_LEFT,
+        BACK_RIGHT
+    }
+
+    public static class Car {
+        protected List<Wheel> wheels;
+
+        public Car() {
+            //init wheels here
+            wheels = new ArrayList<>();
+            for (String loadWheel : loadWheelNamesFromDB())
+                wheels.add(Wheel.valueOf(loadWheel));
+        }
+
+        protected String[] loadWheelNamesFromDB() {
+            //this method returns mock data
+            return new String[]{"FRONT_LEFT", "FRONT_RIGHT", "BACK_LEFT", "BACK_RIGHT"};
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
